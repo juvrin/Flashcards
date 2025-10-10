9a8e23ac1600f055bb3d47f76477761ddf9550e4
Jules <195559278+spacecookie007@users.noreply.github.com>
2025-10-10 12:45:15 +0200

    Added write to csv file and logic to only display second round in case of mistakes in first round

--- main.py
+++ main.py
@@ -1,57 +1,84 @@
 import pandas as pd
-import json
+from pathlib import Path
+import datetime
+import os.path
 
 
-#make sure that the headers are called "QUESTION" and "ANSWER"
-#make sure that you have one row for questions and answers
+# Make sure that the headers are called "QUESTION" and "ANSWER"
+# Make sure that you questions and answers are on the same row but in separate columns
 data = pd.read_csv("Vragen CSS.csv")
 # print(data.head(5))
+
+def printfunc(round, ncorrect, nwrong):
+    if round == 1:
+        print("====== RESULTS FIRST ROUND ========")
+    else: 
+        print("====== RESULTS SECOND ROUND ========")
+    print(f"Correct: {ncorrect}")
+    print(f"Wrong: {nwrong}")
+
 
 correct = {}
 wrong = {}
 for i in range(0,data.shape[0]):
     print(f"\n{data['QUESTION'][i]}")
-    a = input("\tShow answer (a) or quit(q): ")
+    a = input("\tShow answer (a) or quit (q): ")
     if a == "a":
         print(f"\t{data['ANSWER'][i]}")
-    if a == "q":
-        break
+    elif a == "q":
+        printfunc(1, len(correct), len(wrong))  
+        break 
 
-    x = input("\tDid you get this correct (y/n): ")
+    x = input("\tDid you get this correct (y/n) or quit (q): ")
     if x == "y":
         correct[data['QUESTION'][i]] = data['ANSWER'][i]
-    if x == "n":
+    elif x == "n":
         wrong[data['QUESTION'][i]] = data['ANSWER'][i]
+    elif x == "q":
+        printfunc(1, len(correct), len(wrong))
+        break
 
+print("====== FINISHED FIRST ROUND ========")
 
-print("====== DONE ========")
-x = input("Do you want to practice your mistakes (y/n): ")
+if len(wrong) > 0:
+    x = input("Do you want to practice your mistakes (y/n): ")
 
-if x == "y":
-    correct2 = {}
-    wrong2 = {}
+    if x == "y":
+        correct2 = {}
+        wrong2 = {}
 
-    for question, answer in wrong.items():
-        print(f"\n{question}")
-        a = input("\tShow answer (a) or quit(q): ")
-        if a == "a":
-            print(f"\t{answer}")
-        if a == "q":
-            break
-        x = input("\tDid you get this correct (y/n): ")
-        if x == "y":
-            correct2[data['QUESTION'][i]] = data['ANSWER'][i]
-        if x == "n":
-            wrong2[data['QUESTION'][i]] = data['ANSWER'][i]
-elif x == "n":
-    pass
+        for question, answer in wrong.items():
+            print(f"\n{question}")
+            a = input("\tShow answer (a) or quit(q): ")
+            if a == "a":
+                print(f"\t{answer}")
+                
+            elif a == "q":
+                printfunc(2, len(correct2), len(wrong2))
+                    
+            x = input("\tDid you get this correct (y/n): ")
+            if x == "y":
+                correct2[question] = answer
+            if x == "n":
+                wrong2[question] = answer
+            #MOETNOG toevoegen dat als je iets anders dan y of n intypt dat je opnieuw de prompt krijgt om y of n in te vullen
 
-print("====== RESULTS FIRST ROUND ========")
-print(correct)
-print(wrong)
-try:
-    print("====== RESULTS SECOND ROUND ========")
-    print(correct2)
-    print(wrong2)
-except NameError:
-    pass
+        printfunc(2, len(correct2), len(wrong2))
+    elif x == "n":
+        pass
+
+    print("====== FINISHED SECOND ROUND ========")
+
+# Write results to csv
+date = datetime.datetime.now()
+results = f"{date.strftime("%d/%m/%Y")},{len(correct)},{len(wrong)}\n"
+
+file = os.path.isfile("results.csv")
+if file:
+    with open('results.csv','a') as fd:
+        fd.write(results)
+else:
+    with open('results.csv','w') as fd:
+        fd.write("Date, Correct, Wrong\n")
+        fd.write(results)
+
