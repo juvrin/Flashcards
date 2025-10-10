6710f4fdedb7850d1d7fcd46b64592bdb18ba28e
Jules <195559278+spacecookie007@users.noreply.github.com>
2025-10-10 13:16:50 +0200

    Added validation on user input

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os.path
 
 # Make sure that the headers are called "QUESTION" and "ANSWER"
 # Make sure that you questions and answers are on the same row but in separate columns
-data = pd.read_csv("Vragen CSS.csv")
+data = pd.read_csv("Vragen CSS_fortesting.csv")
 # print(data.head(5))
 
 def printfunc(round, ncorrect, nwrong):
@@ -20,25 +20,55 @@ def printfunc(round, ncorrect, nwrong):
 
 correct = {}
 wrong = {}
-for i in range(0,data.shape[0]):
-    print(f"\n{data['QUESTION'][i]}")
-    a = input("\tShow answer (a) or quit (q): ")
-    if a == "a":
-        print(f"\t{data['ANSWER'][i]}")
-    elif a == "q":
-        printfunc(1, len(correct), len(wrong))  
-        break 
+practice = True
+while practice:
+    for i in range(0,data.shape[0]):
 
-    x = input("\tDid you get this correct (y/n) or quit (q): ")
-    if x == "y":
-        correct[data['QUESTION'][i]] = data['ANSWER'][i]
-    elif x == "n":
-        wrong[data['QUESTION'][i]] = data['ANSWER'][i]
-    elif x == "q":
-        printfunc(1, len(correct), len(wrong))
-        break
+        showanswer = True
+        correctwrong = True
+
+        while showanswer:
+            print(f"\n{data['QUESTION'][i]}")
+            a = input("\tShow answer (a) or quit (q): ")
+            if a == "a":
+                print(f"\t{data['ANSWER'][i]}")
+                showanswer = False 
+                break
+            elif a == "q":
+                showanswer = False 
+                correctwrong = False
+                practice = False
+                break
+            else:
+                print("Please enter a or q.")
+                showanswer = True
+            
+        while correctwrong:
+            x = input("\tDid you get this correct (y/n) or quit (q): ")
+            if x == "y":
+                correct[data['QUESTION'][i]] = data['ANSWER'][i]
+                correctwrong = False
+                break
+            elif x == "n":
+                wrong[data['QUESTION'][i]] = data['ANSWER'][i]
+                correctwrong = False
+                break
+            elif x == "q":
+                showanswer = False 
+                correctwrong = False
+                practice = False
+                break
+        
+        if a == "q" or x =="q":
+            practice = False
+            break
+    printfunc(1, len(correct), len(wrong))          
+    practice = False
+          
+    
 
 print("====== FINISHED FIRST ROUND ========")
+
 
 if len(wrong) > 0:
     x = input("Do you want to practice your mistakes (y/n): ")
@@ -46,28 +76,54 @@ if len(wrong) > 0:
     if x == "y":
         correct2 = {}
         wrong2 = {}
+        practice = True
+        while practice:
+            for question, answer in wrong.items():
 
-        for question, answer in wrong.items():
-            print(f"\n{question}")
-            a = input("\tShow answer (a) or quit(q): ")
-            if a == "a":
-                print(f"\t{answer}")
-                
-            elif a == "q":
-                printfunc(2, len(correct2), len(wrong2))
+                showanswer = True
+                correctwrong = True
+                while showanswer:
+                    print(f"\n{question}")
+                    a = input("\tShow answer (a) or quit (q): ")
+                    if a == "a":
+                        print(f"\t{answer}")
+                        showanswer = False 
+                        break
+                    elif a == "q":
+                        showanswer = False 
+                        correctwrong = False
+                        practice = False
+                        break
+                    else:
+                        print("Please enter a or q.")
+                        showanswer = True
                     
-            x = input("\tDid you get this correct (y/n): ")
-            if x == "y":
-                correct2[question] = answer
-            if x == "n":
-                wrong2[question] = answer
-            #MOETNOG toevoegen dat als je iets anders dan y of n intypt dat je opnieuw de prompt krijgt om y of n in te vullen
+                while correctwrong:
+                    x = input("\tDid you get this correct (y/n) or quit (q): ")
+                    if x == "y":
+                        correct2[question] = answer
+                        correctwrong = False
+                        break
+                    elif x == "n":
+                        wrong2[question] = answer
+                        correctwrong = False
+                        break
+                    elif x == "q":
+                        showanswer = False 
+                        correctwrong = False
+                        practice = False
+                        break
+                
+                if a == "q" or x =="q":
+                    practice = False
+                    break
+            printfunc(2, len(correct2), len(wrong2))          
+            practice = False
 
-        printfunc(2, len(correct2), len(wrong2))
-    elif x == "n":
-        pass
+elif x == "n":
+    pass
 
-    print("====== FINISHED SECOND ROUND ========")
+print("====== FINISHED SECOND ROUND ========")
 
 # Write results to csv
 date = datetime.datetime.now()
@@ -81,4 +137,3 @@ else:
     with open('results.csv','w') as fd:
         fd.write("Date, Correct, Wrong\n")
         fd.write(results)
-
