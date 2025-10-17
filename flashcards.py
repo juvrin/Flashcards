86f642805e7849e1836e1fe59baf5a3d6c11e585
Jules <195559278+spacecookie007@users.noreply.github.com>
2025-10-17 22:24:26 +0200

    Added a function to load wrong questions of last time from json and only practice those. Removed the second round logic because not needed anymore

--- flashcards.py
+++ flashcards.py
@@ -5,31 +5,44 @@ import pandas as pd
 import sys
 import datetime
 import os.path
+import json
 
+""""
+MOETNOG:
+- topics anders laten wegschrijven naar .csv en die aparte functie waarbij je het apart inlaadt is niet nodig
+
+===== evt ====
+- vervolgens toevoegen aan homescreen dat je ofwel de vragenlijst.json doorneemt ofwel de vorig_keer_fout.json
+=> door dit als eerste vraag toe te voegen als question text 
+bv "wil je je fouten van vorige keer oefenen? correct = ja, wrong = nee, alles opnieuw doen)"
+"""
 # Adapted version from https://medium.com/@arunabh223/how-i-prepare-for-exams-by-creating-flashcards-using-python-6db823b74083 
 
 # Path to your csv file
 file_path = 'Vragen CSS_fortesting.csv'
 outputfile = "Results_fortesting.csv"
+outputfile_wrong = 'Wrong_last_time_fortesting.json'
 random_option = 'n'
+practice_wrong = 'y'
 
-# Load flashcards from csv file
 def load_flashcards(file_path):
+    """Load flashcards from csv file"""
     df = pd.read_csv(file_path)
-    return dict(zip(df['QUESTION'], df['ANSWER']))
-
-def load_topics(file_path):
-    df_topics = pd.read_csv(file_path)
-    df_topics_only = list(df_topics['TOPIC'])
-    undup = []
-    for i in df_topics_only:
-        while i not in undup:
-            undup.append(i)  
-    return undup
+    dict_list = df.to_dict('records')
+    return dict_list
+    
+def load_wrongs(outputfile_wrong):
+    """Load questions wrongly answered in previous session from json file"""
+    with open(outputfile_wrong, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    return data
 
 # Load flashcards
 flashcards = load_flashcards(file_path)
-topics = load_topics(file_path)
+try:
+    wrong_last_time = load_wrongs(outputfile_wrong)
+except FileNotFoundError:
+    pass
 
 class FlashcardApp:
     def __init__(self, master):
@@ -82,10 +95,15 @@ class FlashcardApp:
         self.roundn = 1
         self.final_wrong = 0
 
-        # Initialize flashcards by random shuffling them
-        self.flashcard_list = list(flashcards.items())
+        # Initialize flashcards
+        if practice_wrong == 'n':
+            self.flashcards = flashcards
+        if practice_wrong == 'y':
+            self.flashcards = wrong_last_time
+
         if random_option == 'y':
-            random.shuffle(self.flashcard_list)
+            random.shuffle(self.flashcards)
+
         self.current_flashcard_index = -1
         self.current_wrong_index = -1
         self.next_flashcard()
@@ -93,33 +111,15 @@ class FlashcardApp:
     def next_flashcard(self):
         self.answer_label.config(text="")
         self.current_flashcard_index += 1
-    
-        if self.current_flashcard_index == len(self.flashcard_list):
-            self.percentage_correct = (self.countcorrect / len(self.flashcard_list))*100
-            self.write_to_csv()
-        if self.current_flashcard_index >= len(self.flashcard_list):
-            self.roundn = 2
-            self.practice_wrongs()
-        else:
-            self.current_question, self.current_answer = self.flashcard_list[self.current_flashcard_index]
-            self.question_label.config(text=self.current_question)
-        self.update_progress()
+        self.percentage_correct = (self.countcorrect / len(self.flashcards))*100
 
-    def practice_wrongs(self):
-        if self.current_wrong_index == -1:
-            self.countcorrect = 0
-            self.countwrong = 0
-            self.progressbar.step(0)
-        if random_option == 'y':
-            random.shuffle(self.wrong)
-        self.answer_label.config(text="")
-        self.current_wrong_index += 1 
-
-        if self.current_wrong_index >= self.final_wrong:
-            self.percentage_correct = (self.countcorrect / len(self.wrong))*100
+        if self.current_flashcard_index == len(self.flashcards):
             self.destroy()
         else:
-            self.current_question, self.current_answer = self.wrong[self.current_wrong_index]
+            self.dict = self.flashcards[self.current_flashcard_index]
+            self.current_question = self.dict['QUESTION']
+            self.current_answer = self.dict['ANSWER']
+            self.current_topic = self.dict['TOPIC']
             self.question_label.config(text=self.current_question)
         self.update_progress()
         
@@ -129,7 +129,7 @@ class FlashcardApp:
     def correct(self, event=None):
         self.countcorrect += 1
         if self.roundn == 1:
-            self.correct.append(self.flashcard_list[self.current_flashcard_index])
+            self.correct.append(self.flashcards[self.current_flashcard_index])
         if self.roundn == 2:
             self.correct.append(self.wrong[self.current_wrong_index])
         self.next_flashcard()
@@ -137,19 +137,19 @@ class FlashcardApp:
     def wrong(self, event=None):
         self.countwrong += 1
         if self.roundn == 1:
-            self.wrong.append(self.flashcard_list[self.current_flashcard_index])
+            self.wrong.append(self.flashcards[self.current_flashcard_index])
             self.final_wrong = len(self.wrong)
         if self.roundn == 2:
             self.wrong.append(self.wrong[self.current_wrong_index])
         self.next_flashcard()
-    
+     
     def destroy(self, event=None):
         self.write_to_csv()
         sys.exit(0)
     
     def update_progress(self):
         self.new_value = self.progress_var.get()
-        self.new_value += 100 / len(self.flashcard_list)
+        self.new_value += 100 / len(self.flashcards)
         if self.new_value > 100:
             self.new_value = 0
         self.progress_var.set(self.new_value)
@@ -158,7 +158,7 @@ class FlashcardApp:
     def write_to_csv(self):
         """Write results to csv"""
         date = datetime.datetime.now()
-        results = f"{date.strftime("%d/%m/%Y %H:%M")},{self.roundn},{self.countcorrect},{self.countwrong}, {int(self.percentage_correct)}, {len(self.flashcard_list)}, {topics}\n"
+        results = f"{date.strftime("%d/%m/%Y %H:%M")},practice wrong? {practice_wrong},{self.countcorrect},{self.countwrong}, {int(self.percentage_correct)}, {len(self.flashcards)}\n"
         file = os.path.isfile(outputfile)
         if file:
             with open(outputfile,'a') as fd:
@@ -167,6 +167,10 @@ class FlashcardApp:
             with open(outputfile,'w') as fd:
                 fd.write("Date, Roundn, Correct, Wrong, Percentage correct, Number of questions, Topics \n")
                 fd.write(results)
+        
+        # Write wrong to json to enable practice later
+        with open(outputfile_wrong, "w") as final:
+            json.dump(self.wrong, final, indent=2, default=lambda x: list(x) if isinstance(x, tuple) else str(x))    
 
         
 root = tk.Tk()
